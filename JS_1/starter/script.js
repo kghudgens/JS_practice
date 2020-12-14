609129270e839d7ff200ca444b82139389dd2141
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-14 23:32:38 +0900

    learned if else statements

--- JS_1/starter/script.js
+++ JS_1/starter/script.js
@@ -1,12 +1,134 @@
-let js = 'amazing';
 
-console.log(40 + 23 +8 - 10);
+{
+// let js = 'amazing';
 
-let firstName = "Kevin"
-console.log(firstName)
+// console.log(40 + 23 +8 - 10);
 
-let myFirstJob = 'Mechanic'
-let myCurrentJob = 'CatMan'
+// let firstName = "Kevin"
+// console.log(firstName)
 
-console.log(myCurrentJob)
-console.log
+// let myFirstJob = 'Mechanic'
+// let myCurrentJob = 'CatMan'
+
+// console.log(myCurrentJob)
+// console.log
+
+// console.log(true)
+// let javascriptIsFun = true
+
+// console.log(javascriptIsFun)
+
+// console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+
+// javascriptIsFun = 'Yes'
+// console.log(typeof javascriptIsFun)
+// console.log( javascriptIsFun)
+
+// let age = 30
+// age =31
+
+// const birthYear = 1995
+
+
+// console.log(ageKevin, ageMomo)
+
+// console.log(ageKevin * 9, ageMomo - 4)
+
+// let x = 10 *5;
+// x += 10 //x + 10
+// x++ // x + 1
+// console.log(x)
+
+// console.log(ageKevin > ageMomo);
+// console.log(ageKevin <= ageMomo);// const now= 2020
+// const ageKevin = now - 1995
+// const ageMomo = now - 1996
+
+// const markWeight = 78 
+// const markHeight = 1.69 
+
+// const johnWeight = 92
+// const johnHeight = 1.95
+
+// const johnBMI = johnWeight/ johnHeight ** 2
+// const markBMI = markWeight/ markHeight ** 2
+
+// const markHigherBMI = markBMI > johnBMI
+// console.log(markHigherBMI)
+
+// const markWeight2 = 95
+// const markHeight2 = 1.88 
+
+// const johnWeight2 = 85
+// const johnHeight2 = 1.76
+
+// const johnBMI2 = johnWeight2/ johnHeight2 ** 2
+// const markBMI2 = markWeight2/ markHeight2 ** 2
+
+// const markHigherBMI2 = markBMI2 > johnBMI2
+// console.log(markHigherBMI2)
+
+// console.log(johnBMI, markBMI)
+// console.log(johnBMI2, markBMI2)
+
+// const firstName = "kevin"
+// const lastName = "hudgens"
+// const birthYear = 1995
+// const year = 2020
+// const job = 'CAT MAN '
+
+// const kevin = "I am " + firstName + " " + lastName+ ", a" + (year - birthYear) + " years old."
+// console.log(kevin)
+
+// const kevinNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`
+// console.log(kevinNew)
+
+// console.log('String with \n\
+// multiple \n\
+// lines')
+
+// console.log(`String with 
+// multiple 
+// lines`)
+
+}
+
+// const age = 13;
+
+// if (age >= 18){
+//     console.log('KEvin can start driving license ')
+// }else{
+//     const yearsLeft =  18 - age
+//     console.log(`Kevin is too young. Wait another ${yearsLeft} years`)
+// }
+
+// const birthYear = 1995
+
+// let century;
+// if (birthYear <= 2000){
+//     century = 20;
+// } else{
+//     century = 21
+// }
+
+// console.log(century)
+
+const markWeight = 78 
+const markHeight = 1.69 
+
+const johnWeight = 92
+const johnHeight = 1.95
+
+const johnBMI = johnWeight/ johnHeight ** 2
+const markBMI = markWeight/ markHeight ** 2
+
+const markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI)
+
+
+if (johnBMI > markBMI){
+    console.log("Johns bmi is greater than marks")
+} else{
+    console.log("Marks bmi is greater than marks")
+}

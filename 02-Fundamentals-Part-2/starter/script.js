cc811b17653c8a781456cc517287d3dd24dabc3e
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-27 15:48:30 +0900

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,3 +8,18 @@
 // const interface = 'Audio'
 // const private = 534
 
+// functions
+function logger(){
+    console.log('My name is Kevin')
+}
+
+logger()
+
+function fruitProcessor(apples, oranges){
+    console.log(apples, oranges)
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice 
+}
+
+const appleJuice =fruitProcessor(2, 4)
+console.log(appleJuice)

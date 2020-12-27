3ff988a2dba745cb28ea69d885c86dfa4db2d4e1
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-27 15:21:27 +0900

    update

--- JS_1/starter/script.js
+++ JS_1/starter/script.js
@@ -266,11 +266,12 @@
 
 // coding challenge
 
-const lowTip = .15
-const highTip = .20
-const mealCost = 430
+// const lowTip = .15
+// const highTip = .20
+// const mealCost = 430
 
-const tip = mealCost >= 50 && mealCost <= 300 ? mealCost * lowTip : mealCost * highTip
-const total = tip + mealCost
+// const tip = mealCost >= 50 && mealCost <= 300 ? mealCost * lowTip : mealCost * highTip
+// const total = tip + mealCost
 
-console.log(`The meal cost was ${mealCost},the tip was ${tip} the total was ${total}` )
+// console.log(`The meal cost was ${mealCost},the tip was ${tip} the total was ${total}` )
+

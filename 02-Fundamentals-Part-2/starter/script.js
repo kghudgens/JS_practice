0b680b2cf4374280a5dd210add347eedcd9e4296
Kevin Hudgens <hudgens1073@gmail.com>
2020-12-27 21:41:00 +0900

    function declaration vs expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -9,17 +9,35 @@
 // const private = 534
 
 // functions
-function logger(){
-    console.log('My name is Kevin')
-}
+// function logger(){
+//     console.log('My name is Kevin')
+// }
 
-logger()
+// logger()
 
-function fruitProcessor(apples, oranges){
-    console.log(apples, oranges)
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-    return juice 
-}
+// function fruitProcessor(apples, oranges){
+//     console.log(apples, oranges)
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice 
+// }
 
-const appleJuice =fruitProcessor(2, 4)
-console.log(appleJuice)
+// const appleJuice =fruitProcessor(2, 4)
+// console.log(appleJuice)
+
+// func expressions vs declarations
+
+// function calcAge1(birthYear){
+//     return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1995 )
+// console.log(age1)
+
+// const calcage2 = function(birthYear){
+//     return 2037 - birthYear
+// }
+
+// const age2 = calcage2(1996)
+
+// console.log(age1)
+// console.log(age2)

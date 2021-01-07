e1aa8e598457dbd0f286e266d32e2b2415592d92
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-07 23:37:44 +0900

    Game logic

--- js_4/script.js
+++ js_4/script.js
@@ -1,17 +1,22 @@
 'use strict'
 
 // console.log(document.querySelector('.message').textContent);
-// // document.querySelector(".message").textContent = 'Correct Number'
+// // document.querySelector(".message").textContent = 'Correct secretNumber'
 
 // console.log(document.querySelector('.message').textContent);
 
-// document.querySelector(".number").textContent = 13
+// document.querySelector(".secretNumber").textContent = 13
 // document.querySelector(".score").textContent = 20
 
 // console.log(document.querySelector(".guess").value); 
 // document.querySelector(".guess").value = 23
 // console.log(document.querySelector(".guess").value); 
 
+const secretNumber = Math.trunc(Math.random() * 20) +1
+document.querySelector(".number").textContent = secretNumber
+
+let score = 20
+let highScore = 0
 
 document.querySelector(".check").addEventListener
 ('click', function(){
@@ -21,6 +26,26 @@ document.querySelector(".check").addEventListener
 
     if (!guess){
         document.querySelector(".message").textContent = 'No Number'
+    }else if(guess === secretNumber){
+        document.querySelector(".message").textContent = 'You win'
+        highScore ++
+
+    }else if(guess> secretNumber){
+        document.querySelector(".message").textContent = 'Guess too high'
+        score--
+        document.querySelector('.score').textContent =score
+
+        if (score< 0){
+            document.querySelector(".message").textContent = 'You lose the game'
+        }
+    }else if(guess < secretNumber){
+        document.querySelector(".message").textContent = 'Guess too low'
+        score--
+        document.querySelector('.score').textContent =score
+
+        if (score< 0){
+            document.querySelector(".message").textContent = 'You lose the game'
+        }
     }
 })
 

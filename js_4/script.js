a8ce69716d9a9a5c35c6314f0e5a8778178b92d5
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-08 22:44:48 +0900

    added functionality to again button

--- js_4/script.js
+++ js_4/script.js
@@ -12,11 +12,19 @@
 // document.querySelector(".guess").value = 23
 // console.log(document.querySelector(".guess").value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 let highScore = 0;
+
+document.querySelector('.again').addEventListener('click', function () {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.message').textContent = 'Start guessing...';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +36,8 @@ document.querySelector('.check').addEventListener('click', function () {
         // When player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'You win';
+        document.querySelector('.number').textContent = secretNumber;
+
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';

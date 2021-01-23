2590d982149d8f3b51be2076e8ca64270a95cdb4
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-23 22:42:28 +0900

    new selectors and function

--- js_4_pig_game/script.js
+++ js_4_pig_game/script.js
@@ -2,10 +2,35 @@
 // Selecting elements
 const score0 = document.querySelector('#score--0');
 const score1 = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
 
 score0.textContent = 0;
 score1.textContent = 0;
+diceEl.classList.add('hidden');
 
-const dice = document.querySelector('.dice');
+// Rolling dice
+btnRoll.addEventListener('click', function () {
+    // 1. Generating random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-dice.classList.add('hidden');
+    // 2.Display Dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // Check for rolled 1; if true swtich to next player
+    if (dice != 1) {
+        // Add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore;
+    } else {
+        // Switch to next player
+    }
+});

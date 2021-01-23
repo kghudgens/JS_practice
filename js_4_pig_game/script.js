3568080467fbfb2a81ff9087864bcb2149dee8c4
Kevin Hudgens <hudgens1073@gmail.com>
2021-01-23 22:26:11 +0900

    selected elements

--- js_4_pig_game/script.js
+++ js_4_pig_game/script.js
@@ -1,1 +1,11 @@
-'use strict'
+'use strict';
+// Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+
+score0.textContent = 0;
+score1.textContent = 0;
+
+const dice = document.querySelector('.dice');
+
+dice.classList.add('hidden');

a54d036e4d57e50511172c755f3050698d727f8d
Kevin Hudgens <hudgens1073@gmail.com>
2021-03-02 23:07:02 +0900

    scope chain example

--- JS_Behind/script.js
+++ JS_Behind/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2021 - birthYear;
+
+    function printAge() {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            const firstName = 'Steven';
+            const str = `Oh, and youre a millenial ${firstName}`;
+            console.log(str);
+
+            const sum = (a, b) => a + b;
+            console.log(sum(4, 5));
+        }
+        console.log(millenial);
+    }
+
+    printAge();
+
+    return age;
+}
+
+const firstName = 'Kevin';
+console.log(calcAge(1995));

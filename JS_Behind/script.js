9431bd74b8e8a2201f66ebe8441b2555434414ab
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-26 19:24:12 +0900

    working with the temporal dead zone

--- JS_Behind/script.js
+++ JS_Behind/script.js
@@ -1,28 +1,42 @@
 'use strict';
 
-function calcAge(birthYear) {
-    const age = 2021 - birthYear;
+// function calcAge(birthYear) {
+//     const age = 2021 - birthYear;
 
-    function printAge() {
-        const output = `${firstName}, you are ${age}, born in ${birthYear}`;
-        console.log(output);
+//     function printAge() {
+//         const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//         console.log(output);
 
-        if (birthYear >= 1981 && birthYear <= 1996) {
-            var millenial = true;
-            const firstName = 'Steven';
-            const str = `Oh, and youre a millenial ${firstName}`;
-            console.log(str);
+//         if (birthYear >= 1981 && birthYear <= 1996) {
+//             var millenial = true;
+//             const firstName = 'Steven';
+//             const str = `Oh, and youre a millenial ${firstName}`;
+//             console.log(str);
 
-            const sum = (a, b) => a + b;
-            console.log(sum(4, 5));
-        }
-        console.log(millenial);
-    }
+//             const sum = (a, b) => a + b;
+//             console.log(sum(4, 5));
+//         }
+//         console.log(millenial);
+//     }
 
-    printAge();
+//     printAge();
 
-    return age;
+//     return age;
+// }
+
+// const firstName = 'Kevin';
+// console.log(calcAge(1995));
+
+console.log(Jonas);
+console.log(job);
+console.log(year);
+
+console.log(addDecl(2, 3));
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+function addDecl(a, b) {
+    return a + b;
 }
-
-const firstName = 'Kevin';
-console.log(calcAge(1995));

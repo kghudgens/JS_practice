318f6792ce7f9a0d9f4a0b254a10e6e218146bda
Kevin Hudgens <hudgens1073@gmail.com>
2021-04-28 22:40:35 +0900

    ways to destructure arrays

--- jsDS/script.js
+++ jsDS/script.js
@@ -7,6 +7,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(startedIndex, mainIndex){
+    return [this.starterMenu[startedIndex], this.mainMenu[mainIndex]]
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -22,3 +26,44 @@ const restaurant = {
     },
   },
 };
+
+const arr = [2,3,4];
+const a  = arr[0]
+const b  = arr[1]
+const c  = arr[2]
+
+// destructuring assignment of array 
+const [x, y, z] = arr 
+console.log(x, y, z);
+
+// original array doesnt get destroyed  
+console.log(arr);
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary =temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main] 
+console.log(main, secondary);
+
+const [started, mainCourse] = restaurant.order(2, 0);
+console.log(started, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested
+// console.log(i, j);
+
+const [i, , [j, k]] = nested
+console.log(i, j, k); 
+
+// Default values 
+const [p=1, q=1, r=1] = [8, 9]
+console.log(p, q, r);
+

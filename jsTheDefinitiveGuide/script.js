54f807fee86ab0b157e665e9402ebb56fde8f339
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-05 12:31:53 +0900

    understanding js classes

--- jsTheDefinitiveGuide/script.js
+++ jsTheDefinitiveGuide/script.js
@@ -3,17 +3,40 @@
 //Arrow functions
 //const myFunc = () =>{}
 
-function printMyName(name) {
-  console.log(name);
+// function printMyName(name) {
+//   console.log(name);
+// }
+
+// const printName = (name) => {
+//   console.log(name);
+// };
+
+// // When returning something you can omit return and just write the statement
+// const multiply = (number) => number * 2;
+
+// printMyName("Kevin");
+// printName("KevinArrow");
+// console.log(multiply(5));
+
+class Huamn {
+  constructor() {
+    this.gender = "male";
+  }
+
+  PrintGender() {
+    console.log(this.gender);
+  }
+}
+class Person extends Huamn {
+  constructor() {
+    super();
+    this.name = "Kevin";
+  }
+  PrintMyName() {
+    console.log(this.name);
+  }
 }
 
-const printName = (name) => {
-  console.log(name);
-};
-
-// When returning something you can omit return and just write the statement
-const multiply = (number) => number * 2;
-
-printMyName("Kevin");
-printName("KevinArrow");
-console.log(multiply(5));
+const person = new Person();
+person.PrintMyName();
+person.PrintGender();

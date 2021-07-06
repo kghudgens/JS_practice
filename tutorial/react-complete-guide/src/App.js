4cea2356dcf25149889b2cdb3f55b24897f3d510
Kevin Hudgens <hudgens1073@gmail.com>
2021-07-06 19:09:59 +0900

    imported the expense item

--- tutorial/react-complete-guide/src/App.js
+++ tutorial/react-complete-guide/src/App.js
@@ -1,5 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h2>Let's get started!</h2>
+
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
